3bf2dcc7b9b9d2038246daf96e4830b45663a5b4
L1MeN9Yu <baal998.mophisto@gmail.com>
2019-11-21 16:53:19 +0800

    Initial Commit

---
+++ Elva/Elva.h
@@ -0,0 +1,19 @@
+//
+//  Elva.h
+//  Elva
+//
+//  Created by Mengyu Li on 2019/11/21.
+//  Copyright © 2019 Mengyu Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Elva.
+FOUNDATION_EXPORT double ElvaVersionNumber;
+
+//! Project version string for Elva.
+FOUNDATION_EXPORT const unsigned char ElvaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Elva/PublicHeader.h>
+
+
