5f09dd9bbe3b09256dd0374a9b3b8ac8936ab901
L1MeN9Yu <baal998.mophisto@gmail.com>
2020-09-01 16:40:31 +0800

    Brotli return type
    Signed-off-by: L1MeN9Yu <baal998.mophisto@gmail.com>

--- Sources/Brotli/Brotli.swift
+++ Sources/Brotli/Brotli.swift
@@ -17,16 +17,19 @@ private extension Brotli {
 // MARK: - File
 
 public extension Brotli {
-    static func compress(inputFile: URL, outputFile: URL, mode: Mode = Mode.default, quality: Quality = Quality.default, windowBits: WindowBits = WindowBits.default) -> Result<Void, Error> {
-        guard FileManager.default.fileExists(atPath: inputFile.path) else { return .failure(.fileNotExist) }
-        guard let fileIn = fopen(inputFile.path, "rb") else { return .failure(.openFile(fileURL: inputFile)) }
+    static func compress(inputFile: URL, outputFile: URL,
+                         mode: Mode = Mode.default, quality: Quality = Quality.default,
+                         windowBits: WindowBits = WindowBits.default) throws
+    {
+        guard FileManager.default.fileExists(atPath: inputFile.path) else { throw Error.fileNotExist }
+        guard let fileIn = fopen(inputFile.path, "rb") else { throw Error.openFile(fileURL: inputFile) }
         let fd = open(outputFile.path, O_CREAT | (true ? 0 : O_EXCL) | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR)
-        guard fd > 0 else { return .failure(.openFile(fileURL: outputFile)) }
-        guard let fileOut = fdopen(fd, "wb") else { return .failure(.openFile(fileURL: outputFile)) }
-        guard let encoderState = BrotliEncoderCreateInstance(nil, nil, nil) else { return .failure(.encoderCreate) }
-        guard BrotliEncoderSetParameter(encoderState, BROTLI_PARAM_MODE, mode.rawValue) == BROTLI_TRUE else { return .failure(.encoderCreate) }
-        guard BrotliEncoderSetParameter(encoderState, BROTLI_PARAM_QUALITY, UInt32(quality.rawValue)) == BROTLI_TRUE else { return .failure(.encoderCreate) }
-        guard BrotliEncoderSetParameter(encoderState, BROTLI_PARAM_LGWIN, UInt32(windowBits.rawValue)) == BROTLI_TRUE else { return .failure(.encoderCreate) }
+        guard fd > 0 else { throw Error.openFile(fileURL: outputFile) }
+        guard let fileOut = fdopen(fd, "wb") else { throw Error.openFile(fileURL: outputFile) }
+        guard let encoderState = BrotliEncoderCreateInstance(nil, nil, nil) else { throw Error.encoderCreate }
+        guard BrotliEncoderSetParameter(encoderState, BROTLI_PARAM_MODE, mode.rawValue) == BROTLI_TRUE else { throw Error.encoderCreate }
+        guard BrotliEncoderSetParameter(encoderState, BROTLI_PARAM_QUALITY, UInt32(quality.rawValue)) == BROTLI_TRUE else { throw Error.encoderCreate }
+        guard BrotliEncoderSetParameter(encoderState, BROTLI_PARAM_LGWIN, UInt32(windowBits.rawValue)) == BROTLI_TRUE else { throw Error.encoderCreate }
         defer {
             BrotliEncoderDestroyInstance(encoderState)
             fclose(fileIn)
@@ -34,7 +37,7 @@ public extension Brotli {
         }
 
         var isEndOfFile = false
-        guard let rawBuffer = malloc(fileBufferSize * 2) else { return .failure(.memory) }
+        guard let rawBuffer = malloc(fileBufferSize * 2) else { throw Error.memory }
         let buffer = rawBuffer.assumingMemoryBound(to: UInt8.self)
         let inputBuffer = buffer
         let outputBuffer = buffer + fileBufferSize
@@ -47,7 +50,7 @@ public extension Brotli {
             if availableInSize == 0 && !isEndOfFile {
                 availableInSize = fread(inputBuffer, 1, fileBufferSize, fileIn)
                 nextInBuffer = UnsafePointer<UInt8>(inputBuffer)
-                guard ferror(fileIn) == 0 else { return .failure(.fileIO) }
+                guard ferror(fileIn) == 0 else { throw Error.fileIO }
                 isEndOfFile = feof(fileIn) == 1
             }
 
@@ -57,13 +60,13 @@ public extension Brotli {
                 &availableInSize, &nextInBuffer,
                 &availableOutSize, &nextOutBuffer, nil
             )
-            guard compressResult == BROTLI_TRUE else { return .failure(.compress) }
+            guard compressResult == BROTLI_TRUE else { throw Error.compress }
 
             if availableOutSize == 0 {
                 let outSize: size_t = nextOutBuffer! - outputBuffer
                 if outSize != 0 {
                     fwrite(outputBuffer, 1, outSize, fileOut)
-                    guard ferror(fileOut) == 0 else { return .failure(.fileIO) }
+                    guard ferror(fileOut) == 0 else { throw Error.fileIO }
                 }
                 availableOutSize = fileBufferSize
                 nextOutBuffer = outputBuffer
@@ -73,31 +76,29 @@ public extension Brotli {
                 let outSize: size_t = nextOutBuffer! - outputBuffer
                 if outSize != 0 {
                     fwrite(outputBuffer, 1, outSize, fileOut)
-                    guard ferror(fileOut) == 0 else { return .failure(.fileIO) }
+                    guard ferror(fileOut) == 0 else { throw Error.fileIO }
                     availableOutSize = 0
                     break
                 }
             }
         }
-
-        return .success(())
     }
 
-    static func decompress(inputFile: URL, outputFile: URL) -> Result<Void, Error> {
-        guard FileManager.default.fileExists(atPath: inputFile.path) else { return .failure(.fileNotExist) }
-        guard let fileIn = fopen(inputFile.path, "rb") else { return .failure(.openFile(fileURL: inputFile)) }
+    static func decompress(inputFile: URL, outputFile: URL) throws {
+        guard FileManager.default.fileExists(atPath: inputFile.path) else { throw Error.fileNotExist }
+        guard let fileIn = fopen(inputFile.path, "rb") else { throw Error.openFile(fileURL: inputFile) }
         let fd = open(outputFile.path, O_CREAT | (true ? 0 : O_EXCL) | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR)
-        guard fd > 0 else { return .failure(.openFile(fileURL: outputFile)) }
-        guard let fileOut = fdopen(fd, "wb") else { return .failure(.openFile(fileURL: outputFile)) }
-        guard let decoderState = BrotliDecoderCreateInstance(nil, nil, nil) else { return .failure(.decoderCreate) }
-        guard BrotliDecoderSetParameter(decoderState, BROTLI_DECODER_PARAM_LARGE_WINDOW, 1) == BROTLI_TRUE else { return .failure(.decoderCreate) }
+        guard fd > 0 else { throw Error.openFile(fileURL: outputFile) }
+        guard let fileOut = fdopen(fd, "wb") else { throw Error.openFile(fileURL: outputFile) }
+        guard let decoderState = BrotliDecoderCreateInstance(nil, nil, nil) else { throw Error.decoderCreate }
+        guard BrotliDecoderSetParameter(decoderState, BROTLI_DECODER_PARAM_LARGE_WINDOW, 1) == BROTLI_TRUE else { throw Error.decoderCreate }
         defer {
             BrotliDecoderDestroyInstance(decoderState)
             fclose(fileIn)
             fclose(fileOut)
         }
         var result: BrotliDecoderResult = BROTLI_DECODER_RESULT_NEEDS_MORE_INPUT
-        guard let rawBuffer = malloc(fileBufferSize * 2) else { return .failure(.memory) }
+        guard let rawBuffer = malloc(fileBufferSize * 2) else { throw Error.memory }
         let buffer = rawBuffer.assumingMemoryBound(to: UInt8.self)
         let inputBuffer = buffer
         let outputBuffer = buffer + fileBufferSize
@@ -108,17 +109,15 @@ public extension Brotli {
 
         while true {
             if result == BROTLI_DECODER_RESULT_NEEDS_MORE_INPUT {
-                if feof(fileIn) == 1 {
-                    return .failure(.fileIO)
-                }
+                if feof(fileIn) == 1 { throw Error.fileIO }
                 availableIn = fread(inputBuffer, 1, fileBufferSize, fileIn)
                 nextInBuffer = UnsafePointer<UInt8>(inputBuffer)
-                guard ferror(fileIn) == 0 else { return .failure(.fileIO) }
+                guard ferror(fileIn) == 0 else { throw Error.fileIO }
             } else if result == BROTLI_DECODER_RESULT_NEEDS_MORE_OUTPUT {
                 let outSize: size_t = nextOutBuffer! - outputBuffer
                 if outSize != 0 {
                     fwrite(outputBuffer, 1, outSize, fileOut)
-                    guard ferror(fileOut) == 0 else { return .failure(.fileIO) }
+                    guard ferror(fileOut) == 0 else { throw Error.fileIO }
                 }
                 availableOut = fileBufferSize
                 nextOutBuffer = outputBuffer
@@ -126,50 +125,48 @@ public extension Brotli {
                 let outSize: size_t = nextOutBuffer! - outputBuffer
                 if outSize != 0 {
                     fwrite(outputBuffer, 1, outSize, fileOut)
-                    guard ferror(fileOut) == 0 else { return .failure(.fileIO) }
+                    guard ferror(fileOut) == 0 else { throw Error.fileIO }
                     availableOut = 0
                     break
                 }
             } else {
-                return .failure(.decompress)
+                throw Error.decompress
             }
 
             result = BrotliDecoderDecompressStream(decoderState, &availableIn, &nextInBuffer, &availableOut, &nextOutBuffer, nil)
         }
-
-        return .success(())
     }
 }
 
 // MARK: - Data
 
 public extension Brotli {
-    static func compress(data: Data, mode: Mode = Mode.default, quality: Quality = Quality.default, windowBits: WindowBits = WindowBits.default) -> Result<Data, Error> {
+    static func compress(data: Data, mode: Mode = Mode.default,
+                         quality: Quality = Quality.default, windowBits: WindowBits = WindowBits.default) throws -> Data
+    {
         let input = data.withUnsafePointer { pointer -> UnsafeRawPointer in UnsafeRawPointer(pointer) }
         let inputBuffer = input.assumingMemoryBound(to: UInt8.self)
         var outputSize: Int = 0
         let maxOutputSize = BrotliEncoderMaxCompressedSize(data.count)
-        guard let outputRawBuffer = malloc(maxOutputSize * MemoryLayout<UInt8>.size) else { return .failure(.memory) }
+        guard let outputRawBuffer = malloc(maxOutputSize * MemoryLayout<UInt8>.size) else { throw Error.memory }
         let outputBuffer = outputRawBuffer.assumingMemoryBound(to: UInt8.self)
-        defer {
-            outputBuffer.deallocate()
-        }
+        defer { outputBuffer.deallocate() }
         outputSize = maxOutputSize
         guard BrotliEncoderCompress(quality.rawValue, windowBits.rawValue, mode.brotliEncoderMode, data.count, inputBuffer, &outputSize, outputBuffer) == BROTLI_TRUE else {
-            return .failure(.compress)
+            throw Error.compress
         }
         let data = Data(bytes: outputBuffer, count: outputSize)
-        return .success(data)
+        return data
     }
 
-    static func decompress(data: Data, bufferCapacity: Int = 1024) -> Result<Data, Error> {
+    static func decompress(data: Data, bufferCapacity: Int = 1024) throws -> Data {
         let input = data.withUnsafePointer { pointer -> UnsafeRawPointer in UnsafeRawPointer(pointer) }
         var availableInSize = data.count
         var nextInputBuffer: UnsafePointer<UInt8>? = input.assumingMemoryBound(to: UInt8.self)
         var outputBufferSize = 0
         var outputBuffer = malloc(bufferCapacity * MemoryLayout<UInt8>.size).assumingMemoryBound(to: UInt8.self)
         defer { outputBuffer.deallocate() }
-        guard let decoderState = BrotliDecoderCreateInstance(nil, nil, nil) else { return .failure(.decoderCreate) }
+        guard let decoderState = BrotliDecoderCreateInstance(nil, nil, nil) else { throw Error.decoderCreate }
         defer { BrotliDecoderDestroyInstance(decoderState) }
         var result: BrotliDecoderResult = BROTLI_DECODER_RESULT_NEEDS_MORE_OUTPUT
         var totalOut: size_t = 0
@@ -186,10 +183,10 @@ public extension Brotli {
         }
 
         if result != BROTLI_DECODER_RESULT_SUCCESS && result != BROTLI_DECODER_RESULT_NEEDS_MORE_INPUT {
-            return .failure(.decompress)
+            throw Error.decompress
         }
 
         let data = Data(bytes: outputBuffer, count: totalOut)
-        return .success(data)
+        return data
     }
 }
--- Tests/ElvaTests/ElvaTests.swift
+++ Tests/ElvaTests/ElvaTests.swift
@@ -8,27 +8,21 @@ final class ElvaTests: XCTestCase {
         let des = try FileManager.default.url(for: .desktopDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
         let input = des.appendingPathComponent("content.json")
         let output = des.appendingPathComponent("content.json.br")
-        try Brotli.compress(inputFile: input, outputFile: output).get()
+        try Brotli.compress(inputFile: input, outputFile: output)
         let decompressOutput = des.appendingPathComponent("content.decompress.json")
-        try Brotli.decompress(inputFile: output, outputFile: decompressOutput).get()
+        try Brotli.decompress(inputFile: output, outputFile: decompressOutput)
     }
 
-    func testBrotliData() {
+    func testBrotliData() throws {
         guard let originalData = "带的2j1儿科2e🤣😊😗都去啊发到你9219额1561".data(using: .utf8) else { fatalError() }
         print("\(originalData.count)")
-        let compressResult = Brotli.compress(data: originalData)
-        switch compressResult {
-        case .failure(let error): print("\(error)")
-        case .success(let data):
+        let data = try Brotli.compress(data: originalData)
+        print("\(data.count)")
+        do {
+            let data = try Brotli.decompress(data: data)
             print("\(data.count)")
-            let decompressResult = Brotli.decompress(data: data)
-            switch decompressResult {
-            case .failure(let error): print("\(error)")
-            case .success(let data):
-                print("\(data.count)")
-                let string = String(data: data, encoding: .utf8)
-                print("\(String(describing: string))")
-            }
+            let string = String(data: data, encoding: .utf8)
+            print("\(String(describing: string))")
         }
     }
 
