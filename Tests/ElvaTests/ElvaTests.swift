9d5cf399db6b120df18ff1455e1fe5755a74fa76
L1MeN9Yu <baal998.mophisto@gmail.com>
2021-07-15 21:24:11 +0800

    [Tests] : add Tests

--- Tests/ElvaTests/ElvaTests.swift
+++ Tests/ElvaTests/ElvaTests.swift
@@ -4,58 +4,52 @@ import XCTest
 @testable import ZSTD
 
 final class ElvaTests: XCTestCase {
+    static let data = Data([0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15])
+
     func testBrotliFile() throws {
-        let des = try FileManager.default.url(for: .desktopDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-        let input = des.appendingPathComponent("content.json")
-        let output = des.appendingPathComponent("content.json.br")
-        try Brotli.compress(inputFile: input, outputFile: output)
-        let decompressOutput = des.appendingPathComponent("content.decompress.json")
-        try Brotli.decompress(inputFile: output, outputFile: decompressOutput)
+        let dataFileURL = URL(fileURLWithPath: "data")
+        try Self.data.write(to: dataFileURL)
+        let compressedFileURL = URL(fileURLWithPath: "data.br")
+        try Brotli.compress(inputFile: dataFileURL, outputFile: compressedFileURL)
+        let decompressFileURL = URL(fileURLWithPath: "data.br.decompressed")
+        try Brotli.decompress(inputFile: compressedFileURL, outputFile: decompressFileURL)
+        let decompressedData = try Data(contentsOf: decompressFileURL)
+        XCTAssertEqual(decompressedData, Self.data)
+        try FileManager.default.removeItem(at: dataFileURL)
+        try FileManager.default.removeItem(at: compressedFileURL)
+        try FileManager.default.removeItem(at: decompressFileURL)
     }
 
     func testBrotliData() throws {
-        guard let originalData = "带的2j1儿科2e🤣😊😗都去啊发到你9219额1561".data(using: .utf8) else { fatalError() }
-        print("\(originalData.count)")
-        let data = try Brotli.compress(data: originalData)
-        print("\(data.count)")
-        do {
-            let data = try Brotli.decompress(data: data)
-            print("\(data.count)")
-            let string = String(data: data, encoding: .utf8)
-            print("\(String(describing: string))")
-        }
+        let compressedData = try Brotli.compress(data: Self.data)
+        let data = try Brotli.decompress(data: compressedData)
+        XCTAssertEqual(data, Self.data)
     }
 
     func testZSTDFile() throws {
-        let des = try FileManager.default.url(for: .desktopDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-        let input = des.appendingPathComponent("content.json")
-        let output = des.appendingPathComponent("content.json.zstd")
-        try ZSTD.compress(inputFile: input, outputFile: output)
-        let decompressOutput = des.appendingPathComponent("content.decompress.json")
-        try ZSTD.decompress(inputFile: output, outputFile: decompressOutput)
+        let dataFileURL = URL(fileURLWithPath: "data")
+        try Self.data.write(to: dataFileURL)
+        let compressedFileURL = URL(fileURLWithPath: "data.zstd")
+        try ZSTD.compress(inputFile: dataFileURL, outputFile: compressedFileURL)
+        let decompressFileURL = URL(fileURLWithPath: "data.zstd.decompressed")
+        try ZSTD.decompress(inputFile: compressedFileURL, outputFile: decompressFileURL)
+        let decompressedData = try Data(contentsOf: decompressFileURL)
+        XCTAssertEqual(decompressedData, Self.data)
+        try FileManager.default.removeItem(at: dataFileURL)
+        try FileManager.default.removeItem(at: compressedFileURL)
+        try FileManager.default.removeItem(at: decompressFileURL)
     }
 
     func testZSTDData() throws {
-        let des = try FileManager.default.url(for: .desktopDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-        let input = des.appendingPathComponent("content.json")
-        let originalData = try Data(contentsOf: input)
-        print("\(originalData.count)")
-        let data = try ZSTD.compress(data: originalData)
-        do {
-            print("\(data.count)")
-            let des = try FileManager.default.url(for: .desktopDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-            let input = des.appendingPathComponent("content.json.zstd")
-            try data.write(to: input)
-            do {
-                let data = try ZSTD.decompress(data: data)
-                print("\(data.count)")
-                let string = String(data: data, encoding: .utf8)
-                print("\(String(describing: string))")
-            }
-        }
+        let compressedData = try ZSTD.compress(data: Self.data)
+        let data = try ZSTD.decompress(data: compressedData)
+        XCTAssertEqual(data, Self.data)
     }
 
     static var allTests = [
-        ("testExample", testBrotliFile),
+        ("testBrotliFile", testBrotliFile),
+        ("testBrotliData", testBrotliData),
+        ("testZSTDFile", testZSTDFile),
+        ("testZSTDData", testZSTDData),
     ]
 }
