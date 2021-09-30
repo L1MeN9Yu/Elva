ec664983a29f34249629591a38c206c7f95830e0
L1MeN9Yu <baal998.mophisto@gmail.com>
2021-09-30 17:39:53 +0800

    [Brotli] : remove deprecated

--- Tests/BrotliTests/BrotliTests.swift
+++ Tests/BrotliTests/BrotliTests.swift
@@ -7,23 +7,6 @@ import Foundation
 import XCTest
 
 final class BrotliTests: XCTestCase {
-    func testBrotliMemory() throws {
-        let originalData = Self.content
-        let compressed: Data = try Brotli.compress(data: originalData)
-        let decompressed: Data = try Brotli.decompress(data: compressed)
-        XCTAssertEqual(originalData, decompressed)
-    }
-
-    func testBrotliFile() throws {
-        let inputFileURL = URL(fileURLWithPath: "brotli_input")
-        let compressFileURL = URL(fileURLWithPath: "brotli_compress")
-        let decompressFileURL = URL(fileURLWithPath: "brotli_decompress")
-        try Self.content.write(to: inputFileURL)
-        try Brotli.compress(inputFile: inputFileURL, outputFile: compressFileURL)
-        try Brotli.decompress(inputFile: compressFileURL, outputFile: decompressFileURL)
-        try XCTAssertEqual(Data(contentsOf: decompressFileURL), Self.content)
-    }
-
     func testMemory() throws {
         func brotli(compressConfig: Brotli.CompressConfig) throws {
             let inputMemory = BufferedMemoryStream(startData: Self.content)
