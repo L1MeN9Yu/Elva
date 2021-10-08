6cebbc23d7ce5cfe10b9fb343df7b404630eddca
L1MeN9Yu <baal998.mophisto@gmail.com>
2021-10-08 11:00:45 +0800

    [Brotli] : remove deprecated error case

--- Sources/Brotli/Brotli.Error.swift
+++ Sources/Brotli/Brotli.Error.swift
@@ -3,16 +3,10 @@
 // Copyright (c) 2019 Mengyu Li. All rights reserved.
 //
 
-import Foundation
-
 public extension Brotli {
     enum Error: Swift.Error {
-        case fileNotExist
-        case openFile(fileURL: URL)
         case encoderCreate
         case decoderCreate
-        case memory
-        case fileIO
         case compress
         case decompress
         case write(expect: Int, written: Int)
