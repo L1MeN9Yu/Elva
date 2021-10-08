520183b3019fd7ee0850566157de5629765e56a9
L1MeN9Yu <baal998.mophisto@gmail.com>
2021-10-08 15:42:34 +0800

    [LZ4] : update tests

--- Tests/LZ4Tests/LZ4Tests.swift
+++ Tests/LZ4Tests/LZ4Tests.swift
@@ -8,34 +8,59 @@ import XCTest
 
 final class LZ4Tests: XCTestCase {
     func testMemory() throws {
-        let inputMemory = BufferedMemoryStream(startData: Self.content)
-        let compressMemory = BufferedMemoryStream()
-        let compressConfig = LZ4.CompressConfig.default
-        try LZ4.compress(reader: inputMemory, writer: compressMemory, config: compressConfig)
-        XCTAssertEqual(compressMemory.size, Self.content.count + 15)
-        let decompressMemory = BufferedMemoryStream()
-        let decompressConfig = LZ4.DecompressConfig.default
-        try LZ4.decompress(reader: compressMemory, writer: decompressMemory, config: decompressConfig)
-        XCTAssertEqual(inputMemory, decompressMemory)
+        func lz4(content: Data, compressConfig: LZ4.CompressConfig) throws {
+            let inputMemory = BufferedMemoryStream(startData: content)
+            let compressMemory = BufferedMemoryStream()
+            let compressConfig = LZ4.CompressConfig.default
+            try LZ4.compress(reader: inputMemory, writer: compressMemory, config: compressConfig)
+            let decompressMemory = BufferedMemoryStream()
+            let decompressConfig = LZ4.DecompressConfig.default
+            try LZ4.decompress(reader: compressMemory, writer: decompressMemory, config: decompressConfig)
+            XCTAssertEqual(inputMemory, decompressMemory)
+        }
+
+        try Self.compressConfigList.forEach { compressConfig in
+            try Self.contents.forEach { content in
+                try lz4(content: content, compressConfig: compressConfig)
+            }
+        }
     }
 
     func testFile() throws {
-        let inputFileURL = URL(fileURLWithPath: "lz4_input")
-        let compressFileURL = URL(fileURLWithPath: "lz4_compress")
-        try Self.content.write(to: inputFileURL)
-        let fileReadStream = FileReadStream(path: inputFileURL.path)
-        let fileWriteStream = FileWriteStream(path: compressFileURL.path)
-        let compressConfig = LZ4.CompressConfig.default
-        try LZ4.compress(reader: fileReadStream, writer: fileWriteStream, config: compressConfig)
-        let decompressFileURL = URL(fileURLWithPath: "lz4_decompress")
-        let compressedReaderStream = FileReadStream(path: compressFileURL.path)
-        let decompressWriterStream = FileWriteStream(path: decompressFileURL.path)
-        let decompressConfig = LZ4.DecompressConfig.default
-        try LZ4.decompress(reader: compressedReaderStream, writer: decompressWriterStream, config: decompressConfig)
-        try XCTAssertEqual(Data(contentsOf: decompressFileURL), Self.content)
+        func lz4(content: Data, compressConfig: LZ4.CompressConfig) throws {
+            let inputFileURL = URL(fileURLWithPath: "lz4_input")
+            let compressFileURL = URL(fileURLWithPath: "lz4_compress")
+            try content.write(to: inputFileURL)
+            let fileReadStream = FileReadStream(path: inputFileURL.path)
+            let fileWriteStream = FileWriteStream(path: compressFileURL.path)
+            let compressConfig = LZ4.CompressConfig.default
+            try LZ4.compress(reader: fileReadStream, writer: fileWriteStream, config: compressConfig)
+            let decompressFileURL = URL(fileURLWithPath: "lz4_decompress")
+            let compressedReaderStream = FileReadStream(path: compressFileURL.path)
+            let decompressWriterStream = FileWriteStream(path: decompressFileURL.path)
+            let decompressConfig = LZ4.DecompressConfig.default
+            try LZ4.decompress(reader: compressedReaderStream, writer: decompressWriterStream, config: decompressConfig)
+            try XCTAssertEqual(Data(contentsOf: decompressFileURL), content)
+        }
+
+        try Self.compressConfigList.forEach { compressConfig in
+            try Self.contents.forEach { content in
+                try lz4(content: content, compressConfig: compressConfig)
+            }
+        }
     }
 }
 
 private extension LZ4Tests {
-    static let content = Data("the quick brown fox jumps over the lazy dog".utf8)
+    static let compressConfigList: [LZ4.CompressConfig] = [
+        LZ4.CompressConfig.default,
+        LZ4.CompressConfig(bufferSize: 2),
+    ]
+}
+
+private extension LZ4Tests {
+    static let contents: [Data] = [
+        Data("the quick brown fox jumps over the lazy dog".utf8),
+        Data((0..<(1 << 12)).map { _ in UInt8.random(in: UInt8.min..<UInt8.max) }),
+    ]
 }
