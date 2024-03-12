02be8034d496f00a3b54a3cdf26408d4acc92819
aone <aone.jga@gmail.com>
2024-03-12 13:52:49 +0100

    ZSTD error code on ZSTD.Error where possible

--- Sources/ZSTD/InternalTypes/CompressContext.swift
+++ Sources/ZSTD/InternalTypes/CompressContext.swift
@@ -20,11 +20,11 @@ final class CompressContext {
 extension CompressContext {
     func set(level: ZSTD.Level) throws {
         let result: Int = ZSTD_CCtx_setParameter(pointer, ZSTD_c_compressionLevel, level.rawValue)
-        if ZSTD.Error.isError(result) { throw ZSTD.Error.setParameter(description: ZSTD.Error.errorName(code: result)) }
+        if ZSTD.Error.isError(result) { throw ZSTD.Error.setParameter(code: result) }
     }
 
     func load(dictionary: ZSTD.Dictionary) throws {
         let result: Int = try ZSTD_CCtx_loadDictionary(pointer, dictionary.pointer, dictionary.size)
-        if ZSTD.Error.isError(result) { throw ZSTD.Error.loadDictionary(description: ZSTD.Error.errorName(code: result)) }
+        if ZSTD.Error.isError(result) { throw ZSTD.Error.loadDictionary(code: result) }
     }
 }
